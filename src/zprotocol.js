f80704e3315d8eb43257119c15a83dad343a848e
kapnak <kapnak.mail@gmail.com>
2024-05-04 18:12:26 +0200

    Add ready variable to wait libsodium to load

--- src/zprotocol.js
+++ src/zprotocol.js
@@ -1,9 +1,12 @@
 const Peer = require('./Peer');
 const RemotePeer = require('./RemotePeer');
 const helpers = require('./helpers');
+const _sodium = require('libsodium-wrappers-sumo');
 
+ready = _sodium.ready;
 
 module.exports = {
+    ready,
     Peer,
     RemotePeer,
     ...helpers
